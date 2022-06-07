71e9fa78505903afee360d8a800631bf2699289f
SouthFox-D <master@southfox.me>
2022-06-07 08:51:26 +0800

    Merge exclusive list from hometown
    Fix exclusive list changing to regular list after rename

--- db/schema.rb
+++ db/schema.rb
@@ -517,6 +517,7 @@ ActiveRecord::Schema.define(version: 2022_05_27_114923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "replies_policy", default: 0, null: false
+    t.boolean "is_exclusive", default: false
     t.index ["account_id"], name: "index_lists_on_account_id"
   end
 
