a44cef994723289d3367b99592491134e01e9902
SouthFox <master@southfox.me>
2022-12-04 18:38:13 +0800

    fix/CW ↩️

--- app/javascript/mastodon/reducers/compose.js
+++ app/javascript/mastodon/reducers/compose.js
@@ -340,7 +340,7 @@ export default function compose(state = initialState, action) {
           spoiler_text = '↩️'.concat(spoiler_text);
         }
         map.set('spoiler', true);
-        map.set('spoiler_text', action.status.get('spoiler_text'));
+        map.set('spoiler_text', spoiler_text);
 
         if (map.get('media_attachments').size >= 1) {
           map.set('sensitive', true);
