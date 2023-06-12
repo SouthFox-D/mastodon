ba9a96382a8c62f7122b0e78e908489573ed3fb6
SouthFox <master@southfox.me>
2023-06-12 11:12:53 +0800

    [db] fix record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_085711) do
+ActiveRecord::Schema.define(version: 2023_06_05_203108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
